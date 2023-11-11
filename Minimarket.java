ee1ca3ddeecc3f6f1bcf9c21438be5d65f94c79b
raihanfryll <132327095+raihanfryll@users.noreply.github.com>
2023-11-11 10:29:48 +0700

    Update Minimarket.java

--- Minimarket.java
+++ Minimarket.java
@@ -5,29 +5,69 @@
  * @author (Willy J.A & Raihan F)
  * @version (11 november 2023)
  */
-public class Minimarket
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class Minimarket {
+    private String name;
+    private int totalProducts;
+    private double totalRevenue;
 
     /**
-     * Konstruktor untuk objek kelas Minimarket
+     * Konstruktor untuk objek kelas Minimarket.
+     * @param name Nama minimarket.
      */
-    public Minimarket()
-    {
-        // initialise instance variables
-        x = 0;
+    public Minimarket(String name) {
+        this.name = name;
+        this.totalProducts = 0;
+        this.totalRevenue = 0.0;
     }
 
     /**
-     * An example of a method 
-     *
-     * @param  quantity  Jumlah produk yang ingin ditambahkan
-     * @return    Total produk dalam minimarket setelah penambahan
+     * Metode untuk menambahkan produk ke minimarket dan menghitung pendapatan.
+     * @param productName Nama produk yang ditambahkan.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void addProduct(String productName) {
+        double price = 0;
+
+        // Tentukan harga berdasarkan jenis produk
+        if (productName.equals("Kecantikan")) {
+            price = 500000;
+        } else if (productName.equals("Pembersih")) {
+            price = 200000;
+        } else if (productName.equals("Konsumsi")) {
+            price = 700000;
+        } else {
+            System.out.println("Produk tidak dikenali: " + productName);
+            return; // Keluar dari metode jika produk tidak dikenali
+        }
+
+        System.out.println("Menambahkan " + productName + " dengan harga " + price);
+        totalProducts++;
+        totalRevenue += price;
+    }
+
+    /**
+     * Metode untuk menampilkan informasi tentang minimarket, termasuk jumlah produk
+     * dan total pendapatan.
+     */
+    public void showInfo() {
+        System.out.println("Selamat datang di " + name);
+        System.out.println("Total produk yang tersedia: " + totalProducts);
+        System.out.println("Total pendapatan: Rp " + totalRevenue);
+    }
+
+    public static void main(String[] args) {
+        Minimarket minimarket = new Minimarket("Minimarket ABC");
+
+        minimarket.addProduct("Kecantikan");
+        minimarket.addProduct("Pembersih");
+        minimarket.addProduct("Konsumsi");
+        minimarket.addProduct("Barang Tidak Dikenali");
+
+        minimarket.showInfo();
+
+        Pegawai pegawai = new Pegawai("John", 2000000);
+        Pembeli pembeli = new Pembeli("Alice", 100000);
+
+        pegawai.displayInfo();
+        pembeli.displayInfo();
     }
 }
